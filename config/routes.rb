3a4802cdbbed04922800dfabaac02dc9c0d83bc3
Dominik Alberski <dominik.alberski@gmail.com>
2017-06-28 16:45:35 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :students do
     get :subjects
+  end
+  root 'visitors#index'
+  resources :teachers do
+    resources :subjects
+  end
+  resources :reports do 
+    resources :subjects
   end
 end
