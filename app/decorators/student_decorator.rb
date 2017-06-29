4f85eac98e58d4aa8190acaa7455208be2bde310
Dominik Alberski <dominik.alberski@gmail.com>
2017-06-29 16:36:58 +0200

    add gem decorator and update method in student_decorator

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,13 +4,13 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
-    sum = 0
+    sum = 0.0
     values = subject_item.subject_item_notes
     return '0.00' if values.size == 0
     values.each do |var|
       sum += var.value
     end
-    avg = sum / values.size.round(3)
-    avg.to_s.ljust(4, '0')
+    avg = sum / values.size
+    avg.round(2).to_s.ljust(4, '0')
   end
 end
