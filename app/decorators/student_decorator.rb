60cda8790d029ea49d273568fdfd785815b8a4ce
Dominik Alberski <dominik.alberski@gmail.com>
2017-06-29 13:06:52 +0200

    add method body to avg_notes in student decorator

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,13 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    sum = 0
+    values = subject_item.subject_item_notes
+    return '0.00' if values.size == 0
+    values.each do |var|
+      sum += var.value
+    end
+    avg = sum / values.size.round(3)
+    avg.to_s.ljust(4, '0')
   end
 end
