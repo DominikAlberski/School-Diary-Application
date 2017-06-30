e8c0caa2b10990892bbe69900e103c59eaee32a3
Dominik Alberski <dominik.alberski@gmail.com>
2017-06-30 13:05:52 +0200

    add payments to reports_controller with routing and view

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,12 @@
 class ReportsController < ApplicationController
   expose(:subject_items) { SubjectItem.includes(:teacher, :students) }
+  expose(:payments)
 
   def subjects
     render :subjects
+  end
+
+  def payments
+    render :payments
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     get :subjects
   end
   get 'reports/subjects', to: 'reports#subjects', as: 'report_subjects'
+  get 'reports/payments', to: 'reports#payments', as: 'report_payments'
   # GET :reports do
   #   get :subjects
   # end
