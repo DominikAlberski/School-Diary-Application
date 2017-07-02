83947a26d07d6ca227c7b0d81388a4e1ae475854
Dominik Alberski <dominik.alberski@gmail.com>
2017-07-03 00:42:37 +0200

    seeds for deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
+require 'faker'
 puts "Seeds: start"
 TEACHER_TITLES = %w(Dr. Prof. TA)
-User.create!(email: 'admin@admin.com',password: 'adminadmin')
+# User.create!(email: 'admin@admin.com',password: 'adminadmin')
 
 3.times do
   Teacher.create!(
